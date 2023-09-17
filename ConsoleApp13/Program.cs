7797ed18595e8c8e90d8e0ad410a9a61bfa9bf67
Elizabeth2803 <elizaart2801@gmail.com>
2023-09-17 21:37:47 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+string a = "28";
+byte b = byte.Parse(a);
+uint c = uint.Parse(a);
+int d = int.Parse(a);
+long e = long.Parse(a);
+double f = double.Parse(a);
+Console.WriteLine(f);
